ab6524da7226f9e121f6698f034dc271e4b2706d
SolidStill <25325131+SolidStill@users.noreply.github.com>
2024-04-19 16:24:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,23 @@
 import streamlit as st
 import cv2
 import numpy as np
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 
 # Import the class labels from labels.txt and assign to a list
 classes = [' '.join(x.split(' ')[1:]).replace('\n','') for x in open('labels.txt', 'r').readlines()]
+
 # Load the Model
-model = load_model('keras_model.h5', compile = False)
+@st.cache(allow_output_mutation=True)
+def load_keras_model():
+    return load_model('keras_model.h5', compile=False)
+
+model = load_keras_model()
 
 # Create the streamlit Title and camera_input
 st.title(f'Is it {classes[0]} or {classes[1]}!?')
 img_file_buffer = st.camera_input(f"Take a picture of {classes[0]} or {classes[1]}")
 
-
-# Trigger when a photo has been taken and the bugger is no longer None
+# Trigger when a photo has been taken and the buffer is no longer None
 if img_file_buffer is not None:
     # Get the image and process it as required by the model
     # We are reshaping and converting the image to match the input the model requires.
@@ -26,14 +30,14 @@ if img_file_buffer is not None:
 
     # We now have the probabilities of the image being for either class
     # Check if either probability is over 80%, if so print the message for that classes.
-    if probabilities[0,0] > 0.8:
-        prob = round(probabilities[0,0] * 100,2)
+    if probabilities[0, 0] > 0.8:
+        prob = round(probabilities[0, 0] * 100, 2)
         st.write(f"I'm {prob}% sure that's {classes[0]}!")
-    elif probabilities[0,1] > 0.8:
-        prob = round(probabilities[0,1] * 100,2)
+    elif probabilities[0, 1] > 0.8:
+        prob = round(probabilities[0, 1] * 100, 2)
         st.write(f"I'm {prob}% sure that's {classes[1]}!")
     else:
-        st.write("I'm not confident that I know what this is! ")
+        st.write("I'm not confident that I know what this is!")
 
     # End on balloons
     st.balloons()
